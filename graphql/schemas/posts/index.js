38b1a17ddd25b4fa63d5ad516eb7a540028cf716
qinye233“$ git config --global user.namgit config --global user.name qinye233“ <Microas@foxmail.com>
2023-10-17 22:38:59 +0800

    :bug:精简代码

--- graphql/schemas/posts/index.js
+++ graphql/schemas/posts/index.js
@@ -2,13 +2,6 @@ const {getRecentPosts} = require("../../src/service/root.service")
 const fs = require("fs")
 const path = require("path")
 
-// const handleData = async () => {
-//   console.log(await getRecentPosts())
-//   return {
-//     recentPosts: await getRecentPosts()
-//   }
-// }
-
 module.exports = {
   resolver: {
     Query: {
