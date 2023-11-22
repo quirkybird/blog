bc077a0b4286d9fbc68d343b7acc17b4140dd068
qinye233“$ git config --global user.namgit config --global user.name qinye233“ <Microas@foxmail.com>
2023-11-22 22:47:39 +0800

    chore: 修改依赖@koa/bodyparser -> koa-bodyparser,启用http

--- graphql/app.js
+++ graphql/app.js
@@ -1,37 +1,44 @@
 const https = require("https")
+const http = require("http")
 const fs = require("fs")
 const { ApolloServer } = require("@apollo/server");
 const { makeExecutableSchema } = require("@graphql-tools/schema");
 const { typeDefs, resolvers } = require("./schemas");
 const {koaMiddleware} = require("@as-integrations/koa")
+const { ApolloServerPluginDrainHttpServer } = require("@apollo/server/plugin/drainHttpServer")
 
 // 导入配置好的app
 const app = require("./src/app");
 // 连接数据库
 require("./database");
-const PORT = 443;
+const PORT = 2333;
+
+//开启https服务 
+// 签名配置
+// const options = {
+//   key: fs.readFileSync("./SSL/private.key"),
+//   cert: fs.readFileSync("./SSL/certificate.crt")
+// }
+// const httpsServer = https.createServer(options, app.callback())
+const httpServer = http.createServer(app.callback());
 
 // 创建 Apollo Server 实例，传入 schema
 const server = new ApolloServer({
   schema: makeExecutableSchema({ typeDefs, resolvers }),
+  plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
 });
 
 // 将 Apollo Server 的中间件应用到 Koa 实例上
 server.start().then((res) => {
   app.use(
+    // '/graphql',
     koaMiddleware(server, {
       context: async ({ ctx }) => ({ token: ctx.headers.token }),
     })
   );
-  //开启https服务 
-  // 签名配置
-  const options = {
-    key: fs.readFileSync("./SSL/private.key"),
-    cert: fs.readFileSync("./SSL/certificate.crt")
-  }
-  const httpsServer = https.createServer(options, app.callback())
+
   // 启动 Koa 实例，监听指定端口
-  httpsServer.listen({ port: PORT }, () => {
+  httpServer.listen({ port: PORT }, () => {
     console.log(
       `Server start at https://localhost:${PORT}`
     );
--- graphql/src/app/index.js
+++ graphql/src/app/index.js
@@ -1,6 +1,7 @@
 const Koa = require("koa");
-const { bodyParser } = require("@koa/bodyparser");
+const bodyParser = require("koa-bodyparser");
 const cors = require("@koa/cors");
+const koaStatic = require("koa-static")
 
 const useRoutes = require("../router/index");
 
@@ -8,10 +9,12 @@ const handle_error = require("./handle_error");
 
 // 创建koa实例
 const app = new Koa();
-// 添加body解析器
-app.use(bodyParser());
 //添加一个cors处理中间件
 app.use(cors());
+// 静态资源访问
+app.use(koaStatic("uploads/"))
+// 添加body解析器
+app.use(bodyParser());
 // 添加路由
 // 这不是一个react-hooks
 // eslint-disable-next-line react-hooks/rules-of-hooks
