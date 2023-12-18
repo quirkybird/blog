a337ab1aed04e23a39e713a1c1565b17e1dc6c3a
qinye233“$ git config --global user.namgit config --global user.name qinye233“ <Microas@foxmail.com>
2023-12-18 23:00:36 +0800

    chore: 添加依赖rehype-raw,支持HTML识别

--- src/components/common/Mkd.js
+++ src/components/common/Mkd.js
@@ -1,6 +1,7 @@
 import { useEffect, useRef } from "react";
 import Markdown from "react-markdown";
 import remarkGfm from "remark-gfm";
+import rehypeRaw from 'rehype-raw';
 import emoji from "remark-emoji";
 import hljs from "highlight.js/lib/common";
 // 引入markdown中代码显示样式，通过highlight.js实现
@@ -34,7 +35,7 @@ const Mkd = ({ markdown }) => {
     <section className="prose max-w-none p-5 prose-img:block lg:prose-p:text-[17.6px]
     prose-img:m-auto prose-h2:text-[16px] lg:prose-pre:text-[16px] prose-pre:text-[14px] dark:prose-code:text-white
      prose-a:text-[#B095DA] dark:prose-invert dark:prose-pre:bg-[#1e293b] prose-pre:bg-[#F2F5F7] prose-code:text-black">
-      <Markdown remarkPlugins={[remarkGfm, emoji]} components={{ pre: preEle, code: codeBlock }}>{markdown}</Markdown>
+      <Markdown remarkPlugins={[remarkGfm, rehypeRaw, emoji]} components={{ pre: preEle, code: codeBlock }}>{markdown}</Markdown>
     </section>
   );
 };
