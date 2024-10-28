ad55b78af81820f0335ffcb78470f37ec92150e5
qinye233 <Microas@foxmail.com>
2024-10-28 22:26:11 +0800

    fix: bugfix

--- src/req/request.js
+++ src/req/request.js
@@ -1,13 +1,15 @@
+const BASE_URL = 'https://blog.yamorz.top';
+
 // 获取所有文章
 export async function getAllPosts() {
-  const raw = await fetch('http://localhost:80/aJson.json');
+  const raw = await fetch(`${BASE_URL}/aJson.json`);
   const res = await raw.json();
   return res;
 }
 
 //获取文章详情
 export async function getPostDetail(id) {
-  const raw = await fetch('http://localhost:80/aJson.json');
+  const raw = await fetch(`${BASE_URL}/aJson.json`);
   const res = await raw.json();
   const onePost = res?.article.filter((item) => String(item.id) === String(id));
   return onePost;
